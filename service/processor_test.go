6f8b17ce62cfb1af11cfbf08629e454e97e7bceb
lsavouillan <lionel.savouillan@gmail.com>
2018-05-08 02:28:44 +0200

    Fixed bug

--- service/processor_test.go
+++ service/processor_test.go
@@ -3,28 +3,28 @@ package service
 import "testing"
 
 func TestProcess7103(t *testing.T) {
-	score := NewProcessingHandler().Process(7, 10, 3).MaxScore
-	if score != 370 {
-		t.Errorf("score found is: %f while it should be 370", score)
+	score := NewProcessingHandler(7, 10, 3).Process().MaxScore
+	if score != 190 {
+		t.Errorf("score found is: %f while it should be 190", score)
 	}
 }
 
 func TestProcess8103(t *testing.T) {
-	score := NewProcessingHandler().Process(8, 10, 3).MaxScore
+	score := NewProcessingHandler(8, 10, 3).Process().MaxScore
 	if score != 180 {
 		t.Errorf("score found is: %f while it should be 180", score)
 	}
 }
 
 func TestProcess9104(t *testing.T) {
-	score := NewProcessingHandler().Process(9, 10, 4).MaxScore
-	if score != 370 {
-		t.Errorf("score found is: %f while it should be 370", score)
+	score := NewProcessingHandler(9, 10, 4).Process().MaxScore
+	if score != 220 {
+		t.Errorf("score found is: %f while it should be 220", score)
 	}
 }
 
 func TestProcess10104(t *testing.T) {
-	score := NewProcessingHandler().Process(10, 10, 4).MaxScore
+	score := NewProcessingHandler(10, 10, 4).Process().MaxScore
 	if score != 180 {
 		t.Errorf("score found is: %f while it should be 180", score)
 	}
